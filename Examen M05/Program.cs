057db3cdda4a1cae1627fb5d86aa8764029e84a2
EricRequena <eric.requena.7e7@itb.cat>
2024-01-08 13:40:21 +0100

    MenuFuntion closes#1Menu

--- Examen M05/Program.cs
+++ Examen M05/Program.cs
@@ -8,5 +8,16 @@ public class Program
     {
    
     }
+    public static void MostrarMenu()
+    {
+        Console.WriteLine("Menu:");
+        Console.WriteLine("a. Validar si un nombre és senar");
+        Console.WriteLine("b. Calcular la potència d'un nombre");
+        Console.WriteLine("c. Retornar un valor aleatori");
+        Console.WriteLine("d. Comptar el nombre de vocals o consonants en un text");
+        Console.WriteLine("e. Sortir");
+        Console.Write("Selecciona una opció: ");
+    }
+
 }
 
